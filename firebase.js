1da3a1a9afe3490ceb939673b75dd6a1014d033f
David182003 <jorgeminano18@gmail.com>
2025-05-03 07:46:51 -0500

    V-0.0.1

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwt9dyCf5iF13R2nhar6F2pKIUkT3Om7Q",
+    authDomain: "paguinasweb-14611.firebaseapp.com",
+    projectId: "paguinasweb-14611",
+    storageBucket: "paguinasweb-14611.appspot.com",
+    messagingSenderId: "1083919701171",
+    appId: "1:1083919701171:web:89d0063a1f9e6e357c4cc6"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
